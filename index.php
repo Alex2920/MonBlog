0171339e284a30a1efa0bd0ab676a910f5e0c89c
Alex2920 <alexleguyadec@gmail.com>
2017-09-19 16:09:32 +0200

    Etape II-A-1

---
+++ index.php
@@ -0,0 +1,11 @@
+
+            <div id="contenu">
+                <?php
+                $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8',
+                        'ts2', 'ts2');
+                $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+                        . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+                        . ' order by BIL_ID desc');
+   // Affichage
+   require 'vueAccueil2.php';
+  
